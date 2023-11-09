25a2613ac09d5c890f3d103f05fd23b11ed8160a
kjungw1025 <kjungw1025@naver.com>
2023-11-09 20:18:55 +0900

    feat: 학식 서비스 로직 추가

---
+++ src/main/java/com/dku/council/domain/cafeteria/repository/CafeteriaInfoRepository.java
@@ -0,0 +1,24 @@
+package com.dku.council.domain.cafeteria.repository;
+
+import com.dku.council.domain.cafeteria.model.dto.response.ResponseCafeteriaInfoDto;
+import com.dku.council.domain.cafeteria.model.entity.CafeteriaInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface CafeteriaInfoRepository extends JpaRepository<CafeteriaInfo, Long> {
+    /*
+    @Query("select new com.dku.council.domain.cafeteria.model.dto.response.ResponseCafeteriaInfoDto(" +
+            "c.mealDate, c.origin, c.allergy" +
+            ") " +
+            "from CafeteriaInfo c where c.mealDate = :mealDate")
+    List<ResponseCafeteriaInfoDto> findByMealDate(@Param("mealDate") LocalDate mealDate);
+     */
+
+    @Query("select c from CafeteriaInfo c where c.mealDate = :mealDate")
+    Optional<CafeteriaInfo> findByMealDate(@Param("mealDate") LocalDate mealDate);
+}
