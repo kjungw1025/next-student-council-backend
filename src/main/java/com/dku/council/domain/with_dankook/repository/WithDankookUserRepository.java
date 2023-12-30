78b98f5a97ed7c1f3ce8e5a1cfac85c403e354e6
kjungw1025 <kjungw1025@naver.com>
2023-12-30 16:13:14 +0900

    feat: 리뷰를 작성할 수 있는지 판단하기 위한 리포지토리, 서비스 기능 등 추가

--- src/main/java/com/dku/council/domain/with_dankook/repository/WithDankookUserRepository.java
+++ src/main/java/com/dku/council/domain/with_dankook/repository/WithDankookUserRepository.java
@@ -6,13 +6,13 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WithDankookUserRepository extends JpaRepository<WithDankookUser, Long>, JpaSpecificationExecutor<WithDankookUser> {
     @Query("select COUNT(u.withDankook.id) " +
             "from WithDankookUser u " +
-            "where u.withDankook.withDankookStatus = 'ACTIVE' and " +
-                    "u.participantStatus = 'VALID' " +
+            "where u.participantStatus = 'VALID' " +
             "group by u.withDankook.id " +
             "having u.withDankook.id = :id ")
     int findRecruitedById(@Param("id") Long id);
@@ -22,4 +22,12 @@ public interface WithDankookUserRepository extends JpaRepository<WithDankookUser
                     "u.participant.id = :userId and " +
                     "u.participantStatus = 'VALID' ")
     Optional<WithDankookUser> isExistsByWithDankookIdAndUserId(Long withDankookId, Long userId);
+
+    @Query("select u from WithDankookUser u " +
+            "where u.withDankook.id = :withDankookId and " +
+                    "u.withDankook.withDankookStatus = 'FULL' and " +
+                    "u.reviewStatus = false and " +
+                    "u.participant.id = :userId and " +
+                    "u.participantStatus = 'VALID' ")
+    Optional<WithDankookUser> checkReviewStatus(Long withDankookId, Long userId);
 }
