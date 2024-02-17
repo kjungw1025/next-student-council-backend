2f58c39a95030810ba749480056a8ab7736a4029
juy4556 <wnstjrtmdgus@gmail.com>
2024-02-17 15:55:50 +0900

    feat: authorize 테스트 코드 작성

---
+++ src/test/java/com/dku/council/domain/oauth/service/OauthServiceTest.java
@@ -0,0 +1,57 @@
+package com.dku.council.domain.oauth.service;
+
+import com.dku.council.domain.oauth.exception.InvalidOauthResponseTypeException;
+import com.dku.council.domain.oauth.exception.OauthClientNotFoundException;
+import com.dku.council.domain.oauth.model.dto.request.OauthRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class OauthServiceTest {
+    @InjectMocks
+    private OauthService oauthService;
+
+    @Test
+    @DisplayName("responseType이 올바르지 않은 경우")
+    void checkInvalidResponseType() {
+        // given
+        String codeChallenge = "codeChallenge";
+        String codeChallengeMethod = "S256";
+        String clientId = "clientId";
+        String redirectUri = "http://localhost:8080/oauth/redirect";
+        String responseType = "not_code";
+        String scope = "nickname email";
+        OauthRequest request = OauthRequest.of(codeChallenge, codeChallengeMethod, clientId,
+                redirectUri, responseType, scope);
+
+        // when, then
+        assertThrows(InvalidOauthResponseTypeException.class, () -> {
+            oauthService.authorize(request);
+        }, "Invalid.oauth-response-type: not_code");
+    }
+
+    @Test
+    @DisplayName("해당 clientId가 존재하지 않는 경우")
+    void checkClientIdNotExist() {
+        // given
+        String codeChallenge = "codeChallenge";
+        String codeChallengeMethod = "S256";
+        String clientId = "notExistClientId";
+        String redirectUri = "http://localhost:8080/oauth/redirect";
+        String responseType = "code";
+        String scope = "nickname email";
+        OauthRequest request = OauthRequest.of(codeChallenge, codeChallengeMethod, clientId,
+                redirectUri, responseType, scope);
+
+        // when, then
+        assertThrows(OauthClientNotFoundException.class, () -> {
+            oauthService.authorize(request);
+        }, "notfound.oauth-client: notExistClientId");
+    }
+}
