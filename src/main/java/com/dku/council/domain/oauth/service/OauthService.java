901cda92a65126b93129954bcb7c36f03233a638
juy4556 <wnstjrtmdgus@gmail.com>
2024-02-22 00:01:37 +0900

    feat: oauth login 로직 추가(로그인 성공 시 authCode반환)

--- src/main/java/com/dku/council/domain/oauth/service/OauthService.java
+++ src/main/java/com/dku/council/domain/oauth/service/OauthService.java
@@ -8,7 +8,14 @@ import com.dku.council.domain.oauth.model.entity.OauthClient;
 import com.dku.council.domain.oauth.model.entity.OauthResponseType;
 import com.dku.council.domain.oauth.repository.OauthClientRepository;
 import com.dku.council.domain.oauth.repository.OauthRedisRepository;
+import com.dku.council.domain.user.exception.WrongPasswordException;
+import com.dku.council.domain.user.model.dto.request.RequestLoginDto;
+import com.dku.council.domain.user.model.entity.User;
+import com.dku.council.domain.user.repository.UserRepository;
+import com.dku.council.domain.user.util.CodeGenerator;
+import com.dku.council.global.error.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -21,6 +28,8 @@ import java.util.UUID;
 public class OauthService {
     private final OauthClientRepository oauthClientRepository;
     private final OauthRedisRepository oauthRedisRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public String authorize(OauthRequest oauthRequest) {
         String clientId = oauthRequest.getClientId();
@@ -34,6 +43,21 @@ public class OauthService {
                 .toUriString();
     }
 
+    @Transactional
+    public OauthLoginResponse login(RequestLoginDto loginInfo, OauthInfo oauthInfo) {
+        checkResponseType(oauthInfo.getResponseType());
+        User user = userRepository.findByStudentId(loginInfo.getStudentId())
+                .orElseThrow(UserNotFoundException::new);
+
+        if (!passwordEncoder.matches(loginInfo.getPassword(), user.getPassword())) {
+            throw new WrongPasswordException();
+        }
+        String authCode = CodeGenerator.generateUUIDCode();
+        Long userId = user.getId();
+        OauthCachePayload cachePayload = oauthInfo.toCachePayload(userId);
+        oauthRedisRepository.cacheOauth(authCode, cachePayload);
+        return OauthLoginResponse.from(authCode);
+    }
     private void checkResponseType(String responseType) {
         String type = OauthResponseType.CODE.getValue();
         if (!responseType.equals(type)) {
