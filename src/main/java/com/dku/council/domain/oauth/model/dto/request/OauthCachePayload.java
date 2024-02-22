f730e3d6ab636da0410dd7b77e759eb94ca75b1a
juy4556 <wnstjrtmdgus@gmail.com>
2024-02-22 17:40:18 +0900

    refactor: OauthCachePayload VO 필드 변경 및 codeChallenge 동등성 체크 로직 추가

--- src/main/java/com/dku/council/domain/oauth/model/dto/request/OauthCachePayload.java
+++ src/main/java/com/dku/council/domain/oauth/model/dto/request/OauthCachePayload.java
@@ -1,17 +1,24 @@
 package com.dku.council.domain.oauth.model.dto.request;
 
+import com.dku.council.domain.oauth.exception.InvalidCodeChallengeException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class OauthCachePayload {
-    String authCode;
+    Long userId;
     String codeChallenge;
     String codeChallengeMethod;
     String scope;
 
-    public static OauthCachePayload of(String authCode, String codeChallenge, String codeChallengeMethod, String scope) {
-        return new OauthCachePayload(authCode, codeChallenge, codeChallengeMethod, scope);
+    public static OauthCachePayload of(Long userId, String codeChallenge, String codeChallengeMethod, String scope) {
+        return new OauthCachePayload(userId, codeChallenge, codeChallengeMethod, scope);
+    }
+
+    public void checkCodeChallenge(String codeChallenge) {
+        if (!this.codeChallenge.equals(codeChallenge)) {
+            throw new InvalidCodeChallengeException(codeChallenge);
+        }
     }
 }
