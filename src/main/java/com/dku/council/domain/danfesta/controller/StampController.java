a754e9b252fa4428609be670f79b612d88e80c83
ChaHyeonMin <gutanbug53@gmail.com>
2024-04-05 02:20:41 +0900

    fix : 관리자 기능은 관리자 페이지로 이동

--- src/main/java/com/dku/council/domain/danfesta/controller/StampController.java
+++ src/main/java/com/dku/council/domain/danfesta/controller/StampController.java
@@ -1,19 +1,13 @@
 package com.dku.council.domain.danfesta.controller;
 
-import com.dku.council.domain.danfesta.model.dto.request.RequestGetStampInfoDto;
-import com.dku.council.domain.danfesta.model.dto.request.RequestStampDto;
-import com.dku.council.domain.danfesta.model.dto.response.ResponseStampForAdminDto;
 import com.dku.council.domain.danfesta.model.dto.response.ResponseStampInfoDto;
 import com.dku.council.domain.danfesta.service.StampService;
 import com.dku.council.global.auth.jwt.AppAuthentication;
-import com.dku.council.global.auth.role.AdminAuth;
 import com.dku.council.global.auth.role.UserAuth;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -42,24 +36,4 @@ public class StampController {
     public ResponseStampInfoDto getMySpecialMission(AppAuthentication auth) {
         return stampService.getStampInfo(auth.getUserId());
     }
-
-//    /**
-//     * 학번으로 사용자별 특별 미션 정보 조회 (Admin)
-//     */
-//    @GetMapping("/{studentId}")
-//    @AdminAuth
-//    public ResponseStampForAdminDto getStudentSpecialMission(AppAuthentication auth,
-//                                                             @Valid @RequestBody RequestGetStampInfoDto dto) {
-//        return stampService.getStampInfoForAdmin(auth.getUserId(), dto.getStudentId());
-//    }
-//
-//    /**
-//     * 특별 미션 스탬프 찍기 (Admin)
-//     */
-//    @PostMapping("/{studentId}")
-//    @AdminAuth
-//    public void stampSpecialMission(AppAuthentication auth,
-//                                    @Valid @RequestBody RequestStampDto dto) {
-//        stampService.stamp(auth.getUserId(), dto.getStudentId(), dto.getBoothNumber());
-//    }
 }
