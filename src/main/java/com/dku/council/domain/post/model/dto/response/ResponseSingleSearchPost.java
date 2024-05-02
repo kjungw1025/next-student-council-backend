b08ebbbc2e5613af64dcede3eb619b713c947c23
ChaHyeonMin <gutanbug53@gmail.com>
2024-05-02 11:21:50 +0900

    feat : 모든 게시물 검색 dto에 슬라이스 기능 추가

--- src/main/java/com/dku/council/domain/post/model/dto/response/ResponseSingleSearchPost.java
+++ src/main/java/com/dku/council/domain/post/model/dto/response/ResponseSingleSearchPost.java
@@ -3,6 +3,8 @@ package com.dku.council.domain.post.model.dto.response;
 import com.dku.council.domain.post.model.entity.Post;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ResponseSingleSearchPost {
 
@@ -10,8 +12,28 @@ public class ResponseSingleSearchPost {
 
     private final String title;
 
+    private final String body;
+
+    private final LocalDateTime createdAt;
+
     public ResponseSingleSearchPost(Post post) {
         this.id = post.getId();
-        this.title = post.getTitle();
+        this.title = sliceTitle(post.getTitle());
+        this.body = sliceBody(post.getBody());
+        this.createdAt = post.getCreatedAt();
+    }
+
+    private String sliceTitle(String title) {
+        if (title.length() > 50) {
+            return title.substring(0, 50);
+        }
+        return title;
+    }
+
+    private String sliceBody(String body) {
+        if (body.length() > 100) {
+            return body.substring(0, 100);
+        }
+        return body;
     }
 }
