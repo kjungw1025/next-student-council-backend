02c924c8cba6301293092132a469a4f6749606e8
ChaHyeonMin <gutanbug53@gmail.com>
2024-05-16 18:56:02 +0900

    fix : Health 체크 시 header log 찍는 코드로 수정

--- src/main/java/com/dku/council/domain/health/HealthController.java
+++ src/main/java/com/dku/council/domain/health/HealthController.java
@@ -3,10 +3,13 @@ package com.dku.council.domain.health;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
 @Tag(name = "Health Check", description = "서버 health 관련 api")
@@ -20,7 +23,7 @@ public class HealthController {
      * health를 체크
      */
     @GetMapping
-    public void healthCheck(){
-        log.info("health check : {}", LocalDateTime.now());
+    public void healthCheck(@RequestHeader MultiValueMap<String, String> headers) {
+        headers.forEach((key, value) -> log.info("Header: {}={}", key, value));
     }
 }
